c1c124e13fc8c5800f34070cf8cde02471ec79bb
mioyday <mioyday@gmail.com>
2023-11-13 21:25:50 +0900

    HelloServlet Request_RequestParamServlet

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -2,6 +2,7 @@ package hello.servlet.basic.request;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Enumeration;
@@ -15,7 +16,8 @@ import java.util.Enumeration;
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
     @Override
-    protected void service(HttpServlet request, HttpServletResponse response) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse
+            resp) throws ServletException, IOException {
         System.out.println("[전체 파라미터 조회] - start");
  /*
  Enumeration<String> parameterNames = request.getParameterNames();
@@ -25,6 +27,22 @@ public class RequestParamServlet extends HttpServlet {
 request.getParameter(paramName));
  }
  */
-
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        System.out.println("request.getParameter(username) = " + username);
+        String age = request.getParameter("age");
+        System.out.println("request.getParameter(age) = " + age);
+        System.out.println();
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        System.out.println("request.getParameterValues(username)");
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("username=" + name);
+        }
+        resp.getWriter().write("ok");
     }
 }

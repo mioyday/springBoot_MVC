788fedd1467dfc5b5c42ec01b5db3ccf8758cea7
mioyday <mioyday@gmail.com>
2023-12-29 21:57:49 +0900

    springmvc_build.gradle

--- src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 import java.util.List;
 /**
  * 클래스 단위 -> 메서드 단위
